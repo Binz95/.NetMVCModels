3ecccc1c848935e5460a3c5c8fdac7da9c1b3161
Binz95 <binianiejacob1995@gmail.com>
2018-11-26 18:01:29 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ashwani()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string str)
+        {
+            ViewBag.Hello = "Hello I am "  + Request.Form["name"] + " located at " + Request.Form["location"];
+            return View();
+        }
+        public ActionResult TextBoxHelper()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TextBoxHelper(string str)
+        {
+            ViewBag.Hello = "Hello " + Request.Form["name"];
+            return View();
+        }
+    }
+}
